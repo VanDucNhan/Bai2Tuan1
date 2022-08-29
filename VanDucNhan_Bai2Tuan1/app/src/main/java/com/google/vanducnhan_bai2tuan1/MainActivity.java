183fb65222bc34816484c19b912f9ff4cb2d9479
VanDucNhan <nyupro123@gmail.com>
2022-08-29 21:15:19 +0700

    commit bai 2 tuan 1

---
+++ VanDucNhan_Bai2Tuan1/app/src/main/java/com/google/vanducnhan_bai2tuan1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.google.vanducnhan_bai2tuan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_tc);
+    }
+}
